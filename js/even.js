fc987400822899bada427ba396b4b8a5403809cf
marikadiblasio <marbla.albram@gmail.com>
2023-03-16 17:08:40 +0100

    add evenOdd function and final winner

--- js/even.js
+++ js/even.js
@@ -11,25 +11,42 @@ console.dir(boxUserChoice);
 const boxUserNum = document.getElementById('userNum');
 console.dir(boxUserNum);
 let pcNum = document.querySelector('.pcNum');
-let sum = document.querySelector('.result');
+let result = document.querySelector('.result');
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 let rndNum = '';
+let sum ='';
 const btn = document.querySelector('button');
-btn.addEventListener('click', printRndNum);
 // Sommiamo i due numeri
-btn.addEventListener('click', addition);
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+let msg = document.querySelector('h2');
+let evenorodd= '';
+
+btn.addEventListener('click', function (){
+    printRndNum()
+    evenOdd(sum);
+    const userChoice = boxUserChoice.value;
+    let end = (userChoice == evenorodd) ? 'vinto' : 'perso';
+    console.log(userChoice, evenorodd)
+    msg.innerHTML = `Hai ${end} questa partita`;
+});
 //Functions
 function printRndNum(){
     rndNum=getRndNumIncl(1,5);
     pcNum.innerText = rndNum;
     addition(userNum,rndNum);
-
 }
 function addition(){
     const userNum = boxUserNum.value;
     console.log(userNum);
-    c= parseInt(userNum) + parseInt(rndNum);
-    sum.innerText = c;
-    console.log(c);
-    return c
+    sum = parseInt(userNum) + parseInt(rndNum);
+    result.innerText = sum;
+    return sum
+}
+function evenOdd(num) {
+    if (num % 2 === 0) {
+        evenorodd = 'even';
+    } else {
+        evenorodd = 'odd';
+    }
+    return evenorodd;
 }
